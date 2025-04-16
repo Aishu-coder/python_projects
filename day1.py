216bba50283edc688bc5679459ec7fd7863d3b5c
Aishu-coder <aishu.macharla@gmail.com>
2025-04-16 20:20:04 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,3 +1,5 @@
+#day1 of my 100 days of python 
+# a simple python code 
 print("Welcome to the band name generator")
 city = input("what is the name of the city that you grew up in ?\n")
 pet = input("what's yorur pets's name ?\n")
