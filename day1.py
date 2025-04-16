3fbcc8841903da01850e50cf74493407af8fe073
Aishu-coder <aishu.macharla@gmail.com>
2025-04-16 20:17:58 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = input("what is the name of the city that you grew up in ?\n")
+pet = input("what's yorur pets's name ?\n")
+print("your nand name could be", city + pet)
