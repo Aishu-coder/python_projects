a5eb76b659a4f0dfa691d0d3259c95da3399b2be
Aishu-coder <aishu.macharla@gmail.com>
2025-04-19 12:13:32 +0530

    Add files via upload

--- day11.py
+++ day11.py
@@ -1,19 +1,21 @@
 from random import choice
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def deal_cards():
 
-    card=choice(cards)
+def deal_cards():
+    card = choice(cards)
     return card
 
+
 def winner(p_score, c_score):
-    if p_score==c_score :
+    if p_score == c_score:
         return "Draw"
-    elif p_score==0:
+    elif p_score == 0:
         return "win with a blackjack."
-    elif c_score ==0:
+    elif c_score == 0:
         return "loose,Opponent wins with a blackjack."
-    elif p_score >21:
+    elif p_score > 21:
         return "loose ,you went over."
     elif c_score > 21:
         return " Win,Opponent went over."
@@ -22,11 +24,12 @@ def winner(p_score, c_score):
     else:
         return " win"
 
-player_cards=[]
-computer_cards=[]
-computer_score=0
-player_score=0
-is_game_over=True
+
+player_cards = []
+computer_cards = []
+computer_score = 0
+player_score = 0
+is_game_over = True
 for _ in range(2):
     player_cards.append(deal_cards())
     computer_cards.append(deal_cards())
@@ -35,10 +38,10 @@ while is_game_over:
     player_score = sum(player_cards)
     computer_score = sum(computer_cards)
     if player_score == 21 and len(player_cards) == 2:
-        player_score=0
-    if computer_score==21 and len(player_cards)==2 :
-        computer_score=0
-    if 11 in player_cards and player_score>21:
+        player_score = 0
+    if computer_score == 21 and len(player_cards) == 2:
+        computer_score = 0
+    if 11 in player_cards and player_score > 21:
         player_cards.remove(11)
         player_cards.append(1)
     print(f"your cards:{player_cards}, current score :{player_score}")
@@ -46,10 +49,10 @@ while is_game_over:
     if player_score == 21 or computer_score == 21 or player_score > 21:
         is_game_over = False
     else:
-        Ask_for_another_card = input("Type 'y' to get another card 'n' to pass ")
+        Ask_for_another_card = input("Type 'y' to get another card 'n' to pass ").strip()
         if Ask_for_another_card == 'y':
             player_cards.append(deal_cards())
-            player_score=sum(player_cards)
+            player_score = sum(player_cards)y
         else:
             is_game_over = False
     while computer_score < 17 and computer_score != 0:
@@ -57,5 +60,5 @@ while is_game_over:
         computer_score = sum(computer_cards)
     print(f"your cards:{player_cards}, current score :{player_score}")
     print(f"computer's final hand:{computer_cards},final score:{computer_score}")
-    print(winner(player_score,computer_score))
-    is_game_over=False
+    print(winner(player_score, computer_score))
+    is_game_over = False
